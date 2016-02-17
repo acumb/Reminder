3b12015539329b31a51a3a8e634d5df84551f297
Alex Cumberworth <alexandercumberwoth@gmail.com>
2016-02-17 16:07:53 +0000

    All core functionality working (sort of).

--- reminder.py
+++ reminder.py
@@ -14,48 +14,157 @@ import argparse
 import pickle
 import datetime
 import os
+import sys
+import subprocess
 import fcntl
 import urwid
+import time
+import pytz
 
 ACTIVE = True
 INACTIVE = False
 MOVE_KEYS = ('up', 'down', 'right', 'left', 'pageup', 'pagedown')
+LONDON_TIMEZONE = pytz.timezone('Europe/London')
     
 
-class Reminder:
-    """Abstract data-type for reminders.
+def lock_and_write_pickle(pickle_file, prepickle):
+    locked = True
+    while locked:
+        try:
+            with open(pickle_file, 'wb') as file:
+                fcntl.fcntl(file, fcntl.LOCK_EX)
+                pickle.dump(prepickle, file)
+                fcntl.fcntl(file, fcntl.LOCK_UN)
+                locked = False
 
-    Data:
+        except OSError:
+            time.sleep(1)
 
-    Methods:
 
-    """
+def lock_and_load_pickle(pickle_file):
+    locked = True
+    while locked:
+        try:
+            with open(pickle_file, 'rb') as file:
+                fcntl.fcntl(file, fcntl.LOCK_EX)
+                pickle_data = pickle.load(file)
+                fcntl.fcntl(file, fcntl.LOCK_UN)
+                locked = False
 
-    def __init__():
-        """ 
+        except OSError:
+            time.sleep(1)
 
-        Arguments:
+    return pickle_data
 
-        """
+def make_filler_time():
+    filler_time = datetime.datetime(2099, 12, 12, hour=23, minute=59, 
+        tzinfo=LONDON_TIMEZONE)
+    return filler_time
+
+class Reminders():
+    """Collection of reminders."""
+
+    def __init__(self):
+        self.data_file = 'test.pickle'
+
+        # If data file doesn't exist, create with filler entry
+        filler_time = make_filler_time()
+        if not os.path.isfile(self.data_file):
+            with open(self.data_file, 'wb') as file:
+                pickle.dump({'fill': filler_time}, file)
+
+        self.reminder_dict = self.__load_reminders()
+
+    def __iter__(self):
+        return iter(self.reminder_dict.items())
+
+    def __len__(self):
+        return len(self.reminder_dict)
+
+    def add_reminder(self, name, reminder_time):
+        self.reminder_dict[name] = reminder_time
+
+    def update_reminder(self, old_name, new_name, new_time):
+        """Update reminder name and time."""
+
+        del self.reminder_dict[old_name]
+        self.reminder_dict[new_name] = new_time
+        lock_and_write_pickle(self.data_file, self.reminder_dict)
+
+    def update(self):
+        self.reminder_dict = self.__load_reminders()
+
+    def delete_reminder(self, name):
+        del self.reminder_dict[name]
+        lock_and_write_pickle(self.data_file, self.reminder_dict)
+
+    def __load_reminders(self):
+        """Load reminders from database."""
+
+        reminder_dict = lock_and_load_pickle(self.data_file)
+
+        return reminder_dict
+
 
 class Daemon:
-    """Background process for checking reminders and spawning popups.
+    """Background process for checking reminders and spawning popups."""
 
-    Data:
+    state_file = 'reminder_daemon_state.pickle'
 
-    Methods:
+    @staticmethod
+    def daemon_running():
+        if not os.path.isfile(Daemon.state_file):
+            daemon_running = False
 
-    """
+        else:
+            daemon_running = lock_and_load_pickle(Daemon.state_file)
 
-    def __init__():
-        """
+        return daemon_running
 
-        Arguments:
+    def __init__(self, reminders):
+        self.reminders = reminders
+        self.daemon_running = True
 
-        """
+        if not os.path.isfile(Daemon.state_file):
+            with open(Daemon.state_file, 'wb') as file:
+                pickle.dump(self.daemon_running, file)
+        else:
+            self.update_state()
 
-class Popup:
-    pass
+    def start(self):
+        # really bad code to make this a background process
+        if os.fork():
+            sys.exit()
+        try:
+            while self.daemon_running:
+                self.reminders.update()
+                self._check_reminders()
+                if not Daemon.daemon_running():
+                    self.daemon_running = False
+
+                time.sleep(10)
+        except:
+            self.daemon_running = False
+            self.update_state()
+            raise
+
+    def _check_reminders(self):
+        reminders_to_delete = []
+        for reminder, reminder_time in self.reminders:
+            if reminder_time <= reminder_time.now(tz=reminder_time.tzinfo):
+                self.popup(reminder)
+                reminders_to_delete.append(reminder)
+
+        for reminder in reminders_to_delete:
+            self.reminders.delete_reminder(reminder)
+
+    def popup(self, name):
+        # It automatically detects what screen is current and spawns there
+        subprocess.run(['urxvt', '--hold', '-e', 'echo', name])
+
+    def update_state(self):
+        lock_and_write_pickle(Daemon.state_file, self.daemon_running)
+
 
 class Urwid_Interface:
     """Urwid interface for creating/editing reminders.
@@ -69,12 +178,8 @@ class Urwid_Interface:
 
     """
 
-    def __init__(self):
+    def __init__(self, reminders):
         """Set up interface."""
-
-        # Set test data, will be replaced with reminder objects data
-        reminder_names = ['Test1', 'Test2']
-        reminder_times = ['Time1', 'Time2']
 
         palette = [
             (None,  '', ''),
@@ -91,25 +196,67 @@ class Urwid_Interface:
         divider = urwid.Divider()
         divider_map = urwid.AttrMap(divider, 'divider')
 
-        list_header_name = urwid.Text(('header', 'Name'), align='left')
-        list_header_name_map = urwid.AttrMap(list_header_name, 'header')
-        list_header_time = urwid.Text(('header', 'Remind at'), align='left')
-        list_header_time_map = urwid.AttrMap(list_header_time, 'header')
-        list_header_body = [list_header_name_map, list_header_time_map]
-        list_header = urwid.Columns(urwid.SimpleListWalker(list_header_body))
+        table_header_name = urwid.Text(('header', 'Name'), align='left')
+        table_header_name_map = urwid.AttrMap(table_header_name, 'header')
+        table_header_time = urwid.Text(('header', 'Remind at'), align='left')
+        table_header_time_map = urwid.AttrMap(table_header_time, 'header')
+        table_header_body = [table_header_name_map, table_header_time_map]
+        table_header = urwid.Columns(urwid.SimpleListWalker(table_header_body))
 
-        list_body = self.__load_reminders()
-        list_widget = urwid.Pile(list_body)
-        top_body = [title_map, divider_map, list_header, list_widget]
-        top_widget = InteractiveListBox(urwid.SimpleFocusListWalker(top_body))
-        top_widget = InteractivePile(top_body)
-        encapsulating_listbox = urwid.ListBox(urwid.SimpleFocusListWalker([top_widget]))
+        menu_header = [title_map, divider_map, table_header]
+        menu_widget = ReminderMenu(menu_header, reminders)
+
         # I don't understand why I need to have the pile enscapsulated in a listbox
-        loop = urwid.MainLoop(encapsulating_listbox, palette)
+        encapsulating_listbox = urwid.ListBox(urwid.SimpleFocusListWalker([menu_widget]))
+        self.loop = urwid.MainLoop(encapsulating_listbox, palette)
 
-        loop.run()
+    def run(self):
+        """Run reminder editor interface."""
 
-    def add_reminder(self, reminder_name, reminder_time):
+        # Consider adding try except block.
+        self.loop.run()
+
+
+# Should I make this only the filter for global keys, and decorate another pile to provide an abstraction for the list of reminders?
+class ReminderMenu(urwid.Pile):
+    """Pile for browsing and editing reminder entries.."""
+
+    def __init__(self, menu_header, reminders):
+        self.state = INACTIVE
+        self.reminders = reminders
+        menu_body = []
+        for name, reminder_time in self.reminders:
+            time_string = self._time_to_string(reminder_time)
+            menu_entry = ReminderEntry(self, name, time_string)
+            menu_body.append(menu_entry)
+        
+        menu_flat = menu_header + menu_body
+        return super(ReminderMenu, self).__init__(menu_flat)
+
+    def keypress(self, size, key):
+        """Decorator to catch custom global option keystrokes"""
+
+        #print('in menu')
+        if self.state == ACTIVE:
+            pass
+        else:
+            if key == 'q':
+                self.quit()
+            elif key == 'n':
+                self.add_reminder()
+            elif key == 'd':
+                self.delete_reminder()
+            elif key == 'k':
+                self.kill_daemon()
+
+        return super(ReminderMenu, self).keypress(size, key)
+
+    def quit(self):
+        """Clean quit."""
+
+        raise urwid.ExitMainLoop()
+
+    def add_reminder(self):
         """Add reminder.
 
         Arguments:
@@ -117,94 +264,160 @@ class Urwid_Interface:
             reminder_time -- Time to remind
 
         """
-
-    def update_reminder(self, reminder_name, reminder_time):
+        name = ''
+        filler_time = make_filler_time()
+        self.reminders.add_reminder(name,filler_time)
+        time_string = self._time_to_string(filler_time)
+        menu_entry = ReminderEntry(self, name, time_string)
+        self.contents.append((menu_entry, self.options()))
+        
+    def update(self, old_name, new_name, time_string):
         """Update reminder."""
 
-    def delete_reminder(self, reminder_name):
+        reminder_datetime = self._string_to_time(time_string)
+        self.reminders.update_reminder(old_name, new_name, reminder_datetime)
+
+    def delete_reminder(self):
         """Delete reminder.
 
         Arguments:
             reminder_name -- 
 
         """
+        reminder_index = self.focus_position
+        # contents are tuples, first position contains widget (wrap for better interface)
+        name = self.contents[reminder_index][0].name
 
-    def run_interface(self):
-        """Run reminder editor interface."""
+        if len(self.reminders) == 1:
+            pass
+            #blank_name = ''
+            #blank_time = ''
+            #self.update(name, blank_name, blank_time)
+        else:
+            del self.contents[reminder_index]
+            self.reminders.delete_reminder(name)
 
-        # Consider adding try except block.
-        self.loop.run()
+    def kill_daemon(self):
+        daemon_running = False
+        lock_and_write_pickle(Daemon.state_file, daemon_running)
+        self.quit()
 
-    def __load_reminders(self):
-        """Load pickled reminders"""
-        for name, time in zip(reminder_names, reminder_times):
+    def _string_to_time(self, datetime_string):
+        date_string, time_string = datetime_string.split()
+        year, month, day = date_string.split('-')
+        hour, minute = time_string.split(':')
+        reminder_datetime = datetime.datetime(int(year), int(month), int(day),
+            hour=int(hour), minute=int(minute), tzinfo=LONDON_TIMEZONE)
+        return reminder_datetime
+
+    def _time_to_string(self, reminder_datetime):
+        year = reminder_datetime.year
+        month = reminder_datetime.month
+        day = reminder_datetime.day
+        hour = reminder_datetime.hour
+        minute = reminder_datetime.minute
+        time_string = '{year}-{month}-{day} {hour}:{minute}'.format(year=year,
+            month=month, day=day, hour=hour, minute=minute)
+        return time_string
 
 
-class SwitchableEdit(urwid.Edit):
-    """Modify Edit widget so editing only between ENTER key presses"""
+class TableEntry(urwid.Edit):
+    """Interface for entries in reminders."""
 
-    def __init__(self, caption='', edit_text=''):
-        """Passes emtpy string as caption."""
-        self.state = INACTIVE
-        super(SwitchableEdit, self).__init__(caption, edit_text=edit_text)
+    name = 0
+    reminder_time = 1
+
+    def __init__(self, reminder_entry, reminder_menu, reminder_data, reminder_type):
+        assert reminder_type in (TableEntry.name, TableEntry.reminder_time)
+
+        self.reminder_entry = reminder_entry
+        self.reminder_menu = reminder_menu
+        self.reminder_type = reminder_type
+        super(TableEntry, self).__init__(caption='', edit_text=reminder_data)
+
+    @property
+    def reminder_data(self):
+        reminder_data = self.get_edit_text()
+        return reminder_data
 
     def keypress(self, size, key):
-        # Should also update the datastructure
+        """Catches keys based on state of table entry."""
+
+        #print('in entry')
         empty_key = ''
-        if self.state == ACTIVE:
-            if key == 'enter':
-                self.state = INACTIVE
+        state_switch_key = 'enter'
+
+        if self.reminder_menu.state == ACTIVE:
+            if key == state_switch_key:
+                self.reminder_menu.state = INACTIVE
+                self.reminder_entry.update(self.reminder_data, self.reminder_type)
                 key = empty_key
             elif key in MOVE_KEYS:
                 key = empty_key
             else:
                 pass
         else:
-            if key == 'enter':
-                self.state = ACTIVE
+            if key == state_switch_key:
+                self.reminder_menu.state = ACTIVE
                 key = empty_key
             elif key not in MOVE_KEYS:
                 key = empty_key
-        return super(SwitchableEdit, self).keypress(size, key)
 
-class InteractivePile(urwid.Pile):
-    """Provide single keystroke global options."""
+        return super(TableEntry, self).keypress(size, key)
 
-    def keypress(self, size, key):
-        """Decorator to catch custom global option keystrokes"""
 
-        if key == 'q':
-            self.quit()
-        elif key == 'n':
-            self.add_reminder()
-        return super(InteractivePile, self).keypress(size, key)
+class ReminderEntry(urwid.Columns):
+    """Interface for reminder entries"""
 
-    def quit():
-        """Clean quit."""
+    def __init__(self, reminder_menu, name, time_string):
+        self.reminder_menu = reminder_menu
+        self.name = name
+        self.time_string = time_string
 
-        raise urwid.ExitMainLoop()
+        table_entry_name = TableEntry(self, reminder_menu, name, TableEntry.name)
+        table_entry_name_map = urwid.AttrMap(table_entry_name, 'options_fill', 'focus')
+        table_entry_time = TableEntry(self, reminder_menu, time_string, TableEntry.reminder_time)
+        table_entry_time_map = urwid.AttrMap(table_entry_time, 'options_fill', 'focus')
+        table_entry_body = [table_entry_name_map, table_entry_time_map]
+        super(ReminderEntry, self).__init__(urwid.SimpleFocusListWalker(table_entry_body))
 
-    def add_reminder()
-        """Update menu and datastructure"""
-
-            list_entry_name = SwitchableEdit(edit_text=name)
-            list_entry_name_map = urwid.AttrMap(list_entry_name, 'options_fill', 'focus')
-            list_entry_time = SwitchableEdit(edit_text=time)
-            list_entry_time_map = urwid.AttrMap(list_entry_time, 'options_fill', 'focus')
-            list_entry_body = [list_entry_name_map, list_entry_time_map]
-            list_entry = urwid.Columns(urwid.SimpleFocusListWalker(list_entry_body))
-            list_body.append(list_entry)
-        list_entry_name = SwitchableEdit(edit_text='Fill in.')
-        list_entry_time = SwitchableEdit(edit_text='Fill in.')
-        list_entry_body = [list_entry_name, list_entry_time]
-        list_entry = urwid.Columns(urwid.SimpleFocusListWalker(list_entry_body))
-        self.contents[4][0].contents.append((list_entry, self.options()))
+    def update(self, reminder_data, reminder_type):
+        """Update underlying data type for reminders."""
         
+        old_name = self.name
+        if reminder_type == TableEntry.name:
+            old_name = self.name
+            self.name = reminder_data
+        elif reminder_type == TableEntry.reminder_time:
+            self.time_string = reminder_data
+
+        self.reminder_menu.update(old_name, self.name, self.time_string)
+
+
 def main():
-    """Check flags and act accordingly."""
+    """Check mode and state and act accordingly."""
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--daemon', dest='daemon_mode', action='store_true')
+    arguments = parser.parse_args()
+    
+    daemon_running = Daemon.daemon_running()
+    reminders = Reminders()
 
-#    Urwid_Interface()
+    if arguments.daemon_mode:
+        if daemon_running:
+            print('\nDaemon already running\n')
+            sys.exit()
+        daemon = Daemon(reminders)
+        daemon.start()
+
+    else:
+        if not daemon_running:
+            print('\nDaemon not running\n')
+            sys.exit()
+        interface = Urwid_Interface(reminders)
+        interface.run()
+
 
 if __name__ == '__main__':
     main()
